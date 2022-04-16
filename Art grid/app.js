6a7bea8a9c11608b86f0f0a17ccd1b883c49a1e0
nitishsaini706 <nitishsaini706@gmail.com>
2022-04-16 17:46:32 +0530

    working on artgrid

--- Art grid/app.js
+++ Art grid/app.js
@@ -1,6 +1,22 @@
-const grid = document.querySelector('.grid');
+const gri = document.querySelector('.gri');
+const board = document.querySelector('.board');
+const grids=[];
+const ele = document.createDocumentFragment();
 
-for(let i=1;i<11;i++)
+
+
+for(let i=0;i<10;i++)
 {
+    const g = document.createElement('i');
+    g.className =`grid`;
+    // grids.push(ele);
+    grids.push(g);
+    ele.append(grids);
     
 }
+
+board.appendChild(ele);
+
+
+// board.append(grids);
+
