a7d41ea1e95b0b2f77ab401a211f8a933f8fecfe
nitishsaini706 <nitishsaini706@gmail.com>
2022-04-20 15:55:29 +0530

    added grids dynamically

--- Art grid/app.js
+++ Art grid/app.js
@@ -1,21 +1,42 @@
-const gri = document.querySelector('.gri');
 const board = document.querySelector('.board');
 const grids=[];
 const ele = document.createDocumentFragment();
 
 
 
-for(let i=0;i<10;i++)
+for(let i=0;i<100;i++)
 {
-    const g = document.createElement('i');
-    g.className =`grid`;
+    const g = document.createElement('div');
+    g.classList.add(`grid`);
+    g.setAttribute('data-id',`${i}`);
     // grids.push(ele);
     grids.push(g);
-    ele.append(grids);
+    ele.append(g);
     
 }
 
 board.appendChild(ele);
+
+const grid = document.querySelectorAll('.grid');
+
+grid.forEach(function(g)
+{
+
+    if(g.dataset.id > 89)
+    {
+        g.style.color = `#454545`
+    }
+    g.addEventListener('mouseover',function()
+    {
+        console.log(g.style.color);
+    })
+})
+
+// grids.forEach(function(g)
+// {
+//     console.log(g.classList.dataset);
+// })
+
 
 
 // board.append(grids);
